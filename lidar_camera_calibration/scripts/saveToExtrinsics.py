35a28039069c196baa34f22227afa8f923964850
Dhruv Agarwal <dhruvagarwal@berkeley.edu>
2024-07-18 05:32:15 +0000

    changed head

---
+++ lidar_camera_calibration/scripts/saveToExtrinsics.py
@@ -0,0 +1,6 @@
+import numpy as np
+np.save("/home/dhruvagarwal/Desktop/ros2_ws/src/lidar_camera_calibration/calibration_data/lidar_camera_calibration/euler.npy", np.array([2.338816443976044, -0.574322815165147, -0.904944999401531], dtype=np.float32))
+np.save("/home/dhruvagarwal/Desktop/ros2_ws/src/lidar_camera_calibration/calibration_data/lidar_camera_calibration/R.npy", np.array([[ 0.51862066, -0.78770242, -0.33250189],
+ [-0.66022288, -0.12185,    -0.74111964],
+ [ 0.54326638,  0.60388531, -0.58325309]], dtype=np.float32))
+np.save("/home/dhruvagarwal/Desktop/ros2_ws/src/lidar_camera_calibration/calibration_data/lidar_camera_calibration/T.npy", np.array([-2.08619504,  1.61377784,  0.77802758], dtype=np.float32))
