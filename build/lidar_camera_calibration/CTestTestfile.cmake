edc94b9676c756ae167e55134536af1de3a3d6ce
Dhruv Agarwal <dhruvagarwal@berkeley.edu>
2024-07-18 05:47:35 +0000

    removing some files

--- build/lidar_camera_calibration/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruvagarwal/Desktop/ros2_ws/src/lidar_camera_calibration
-# Build directory: /home/dhruvagarwal/Desktop/ros2_ws/build/lidar_camera_calibration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
